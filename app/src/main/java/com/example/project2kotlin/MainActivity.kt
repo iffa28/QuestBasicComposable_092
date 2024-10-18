1caf33b3da6ccbecd4573938f658e72df7e8063a
Iffatuz Zahra <iffatuz.zahra.ft22@mail.umy.ac.id>
2024-10-18 19:11:53 +0700

    menambahkan fungsi untuk menampilkan gambar

--- app/src/main/java/com/example/project2kotlin/MainActivity.kt
+++ app/src/main/java/com/example/project2kotlin/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -15,6 +16,7 @@ import androidx.compose.runtime.ComposableInferredTarget
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -57,9 +59,12 @@ fun BasicCompose(modifier: Modifier = Modifier){
                 fontSize = 25.sp,
                 fontWeight = FontWeight.Bold,
                 color = Color.Black,
-
-                ),
+            ),
         )
+
+        Image(painter = painterResource(id =R.drawable.fifa),
+            contentDescription = null ) //atau ""
+
     }
 
 
