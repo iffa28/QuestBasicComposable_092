58a73754b1368b41122df4cea4123b8fd6bbdd3e
Iffatuz Zahra <iffatuz.zahra.ft22@mail.umy.ac.id>
2024-10-18 19:00:24 +0700

    menambahkan text

--- app/src/main/java/com/example/project2kotlin/MainActivity.kt
+++ app/src/main/java/com/example/project2kotlin/MainActivity.kt
@@ -14,7 +14,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.ComposableInferredTarget
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.project2kotlin.ui.theme.Project2KotlinTheme
 
 class MainActivity : ComponentActivity() {
@@ -39,6 +43,11 @@ fun BasicCompose(modifier: Modifier = Modifier){
     Column(horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center,
         modifier = Modifier.fillMaxSize())
+    {
+        Text(text = "Indonesia",
+
+        )
+    }
 
 
 }}
