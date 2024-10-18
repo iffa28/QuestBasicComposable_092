813c272e4f115276c640bb6312cc8e9bd07a54d0
Iffatuz Zahra <iffatuz.zahra.ft22@mail.umy.ac.id>
2024-10-18 18:57:46 +0700

    membuat layout dengan susunan seperti kolom

--- app/src/main/java/com/example/project2kotlin/MainActivity.kt
+++ app/src/main/java/com/example/project2kotlin/MainActivity.kt
@@ -4,12 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.ComposableInferredTarget
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.project2kotlin.ui.theme.Project2KotlinTheme
@@ -32,6 +35,10 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun BasicCompose(modifier: Modifier = Modifier){
+
+    Column(horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.fillMaxSize())
 
 
 }}
