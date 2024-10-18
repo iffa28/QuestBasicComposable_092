9cb7bf2c688a39faa886ccd69a9389644c9e6f90
Iffatuz Zahra <iffatuz.zahra.ft22@mail.umy.ac.id>
2024-10-18 18:38:35 +0700

    Initial commit

---
+++ app/src/main/java/com/example/project2kotlin/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.project2kotlin
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.project2kotlin.ui.theme.Project2KotlinTheme
+
+class MainActivity : ComponentActivity() {
+    
+}
+
